72a0e95ed0108175d4cf52dc4b96b64be1644eee
Martin P. Hellwig <martin.hellwig@gmail.com>
2016-07-02 12:15:37 +0100

    bump version for testing auto deployment.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 NAME = 'BBCS-Tools'
 DESCRIPTION = 'Bitbucket Codeship integration tools.'
-VERSION = '0.0.0.10'
+VERSION = '0.0.0.11'
 LICENSE = 'BSD'
 AUTHOR = 'Martin P. Hellwig'
 AUTHOR_EMAIL = 'martin.hellwig@gmail.com'
