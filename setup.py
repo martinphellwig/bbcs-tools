cc3eceafc0b0cc2d0c872eb0fa62f86a4876b474
Martin P. Hellwig <martin.hellwig@gmail.com>
2016-07-02 02:34:56 +0100

    just bump version so I can test the pypi upload script.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 NAME = 'BBCS-Tools'
 DESCRIPTION = 'Bitbucket Codeship integration tools.'
-VERSION = '0.0.0.6'
+VERSION = '0.0.0.7'
 LICENSE = 'BSD'
 AUTHOR = 'Martin P. Hellwig'
 AUTHOR_EMAIL = 'martin.hellwig@gmail.com'
