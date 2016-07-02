18a51149516c87bf22410a14556780a91dcc7023
Martin P. Hellwig" <martin.hellwig@gmail.com>
2016-07-02 02:21:24 +0100

    and bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 NAME = 'BBCS-Tools'
 DESCRIPTION = 'Bitbucket Codeship integration tools.'
-VERSION = '0.0.0.5'
+VERSION = '0.0.0.6'
 LICENSE = 'BSD'
 AUTHOR = 'Martin P. Hellwig'
 AUTHOR_EMAIL = 'martin.hellwig@gmail.com'
